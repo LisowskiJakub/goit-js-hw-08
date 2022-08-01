dbb4af7c582e30399eea692f08b11f2482adbbf7
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-08-01 22:01:12 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,8 +5,9 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
 galleryItems.forEach(item => {
     const markup = `<a class="gallery__item" href="${item.original}">
       <img class="gallery__image" src="${item.original}" data-source="${item.original}" alt="${item.description}" title="${item.description}"/>
