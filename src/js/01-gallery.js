84ae6314361e9711ad9b27c0da2f6bb135334b32
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-08-01 21:54:26 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+galleryItems.forEach(item => {
+    const markup = `<a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.original}" data-source="${item.original}" alt="${item.description}" title="${item.description}"/>
+      </a> `
+  
+  
+    gallery.insertAdjacentHTML('beforeend', markup);
+  
+  })
+  new SimpleLightbox(".gallery a", { 
+    captionDelay: 250,
+    overlayOpacity:0.97
+  
+  });
