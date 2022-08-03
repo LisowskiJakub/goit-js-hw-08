e7b01e05163bc5dab30a1108989086521b7a072b
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-08-03 21:46:39 +0200

    last commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,23 +5,27 @@ const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 let lastSession = {};
 
+form.addEventListener('input', throttle(
+    () => {
+        localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }))
+    }
+    , 500)
+);
+
 try {
     lastSession = JSON.parse(localStorage.getItem('feedback-form-state'));
-
+    if(lastSession!==null){
+        email.value=lastSession.email;
+        message.value=lastSession.message;
+     }
 } catch (err) {
     console.log('Parse error:' + err.message);
 }
 
-form.addEventListener('input', throttle(
-    () => {
-        localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }))
-        lastSession= {email:"email.Value", message:"message.value"}
-    }
-    , 500)
-);
 form.addEventListener('submit', e => {
     e.preventDefault();
-    lastSession==null ? undefined : console.log(lastSession); 
-    localStorage.removeItem('feedback-form-state');
+    console.log({ email: email.value, message: message.value });
     form.reset();
+    localStorage.removeItem('feedback-form-state');
+
 })
