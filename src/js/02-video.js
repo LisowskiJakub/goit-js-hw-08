16d91d3055ce8e8bb583f5c6a37b2dfee78495c3
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-08-08 20:07:47 +0200

    fixed on pause localStorage update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,23 @@
 
-   import { throttle } from 'lodash';
+import { throttle } from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-  const iframe = document.querySelector('iframe');
-  const player = new Vimeo.Player(iframe);
+player.on('play', function () {
+    console.log('played the video!');
+});
+i
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
 
-  player.on('play', function() {
-      console.log('played the video!');
-  });
-
-  player.getVideoTitle().then(function(title) {
-      console.log('title:', title);
-  });
-
-
-  
-
-
-player.on('timeupdate', throttle(({ duration, percent, seconds })=>{
-localStorage.setItem("videoplayer-current-time",`${seconds}`)},500)
+});
+player.getCurrentTime('timeupdate', throttle((seconds) => {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`)
+}, 500)
 );
 
- player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).catch(function(error) {
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).catch(function (error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
