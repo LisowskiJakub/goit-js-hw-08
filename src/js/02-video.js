5f9ce07f797ffee88c7ebc845892798357e35a9e
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-08-09 05:09:26 +0200

    fixed on pause localStorage update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,20 @@
 
-import { throttle } from 'lodash';
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+   import { throttle } from 'lodash';
+  const iframe = document.querySelector('iframe');
+  const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-    console.log('played the video!');
-});
-i
-player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
+  player.on('play', function() {
+      console.log('played the video!');
+  });
 
-});
-player.getCurrentTime().then(function(seconds) {
-    throttle((sec) => {
-        localStorage.setItem("videoplayer-current-time", `${sec}`)
-    }, 500)
-    // seconds = the current playback position
-}).catch(function(error) {
-    // an error occurred
-});
-player.getCurrentTime(throttle((seconds) => {
-    localStorage.setItem("videoplayer-current-time", `${seconds}`)
-}, 500)
+  player.getVideoTitle().then(function(title) {
+      console.log('title:', title);
+  });
+player.getCurrentTime('timeupdate', throttle(({ duration, percent, seconds })=>{
+localStorage.setItem("videoplayer-current-time",`${seconds}`)},500)
 );
 
-
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).catch(function (error) {
+ player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
