7b8d83f40360bf34e00848d6fa1be5c292929604
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-03-06 18:51:12 +0100

    resolved task nr 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Change code below this line
+const {log} = console;
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map((item)=>`
+<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} title=${item.description} />
+</a>
+`)
+.join('')
+
+gallery.insertAdjacentHTML('beforeend', markup)
+let lightbox= new SimpleLightbox('.gallery a',{
+    captionDelay: 250,
+});
